2d3c57d21e1838ba6b4a61ccf105cd711fa32fcf
roger yee <ryee.epicodus@gmail.com>
2018-04-06 12:16:44 -0700

    updates tsconfig.json and .gitignore

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export let masterFirebaseConfig = {
+  apiKey: 'AIzaSyCYM5py71Jg1iF9BlYKY1jPHF7jeTmUXyE',
+  authDomain: 'trellangular.firebaseapp.com',
+  databaseURL: 'https://trellangular.firebaseio.com',
+  projectId: 'trellangular',
+  storageBucket: 'trellangular.appspot.com',
+  messagingSenderId: '940272338842'
+};
